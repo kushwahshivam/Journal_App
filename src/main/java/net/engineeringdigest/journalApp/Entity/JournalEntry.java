68683cf360cb9dfa083dddc4651b2a8c3bb51e8d
kushwahshivam <shivamkushwah97522@gmail.com>
2025-05-04 19:39:48 +0530

    initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
@@ -0,0 +1,35 @@
+package net.engineeringdigest.journalApp.Entity;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
